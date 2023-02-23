fd024c18c355e5fb7178df443e3ec24bed9a6156
atmoner <contact.atmoner@gmail.com>
2023-02-23 17:17:30 +0100

    Config network

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,5 +71,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  server: {
+    // default: localhost
+    host: '0',
+    port: 3000,
+  },  
 }
